36a84e1a2ea81299372a5f6a6bf86ebf24e0e902
Yannick Scherer <yannick.scherer@futurice.com>
2020-12-23 20:22:12 +0100

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-isolate "0.2.0"
+(defproject lein-isolate "0.2.1-SNAPSHOT"
   :description "Provide isolation for your plugins."
   :url "https://github.com/xsc/lein-isolate"
   :license {:name "MIT License"
