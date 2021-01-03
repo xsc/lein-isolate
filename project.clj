762e71926407bbac58dee84cbf839fb0139c0cdb
Yannick Scherer <yannick.scherer@futurice.com>
2021-01-03 12:05:00 +0100

    Upgrade 'mranderson' to 0.5.2

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "none"
             :year 2018
             :key "mit"}
-  :dependencies [[thomasa/mranderson "0.5.1"]]
+  :dependencies [[thomasa/mranderson "0.5.2"]]
   :eval-in :leiningen)
